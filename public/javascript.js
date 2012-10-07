79ac4cc37ecc3a35b70dfa3e3fc00ea210cb9bf4
Solomon laptop <solomon.kahn@gmail.com>
2012-10-07 14:12:25 -0400

    successfully adding message to photo, and wrapping text

--- public/javascript.js
+++ public/javascript.js
@@ -1,8 +1,64 @@
 $(document).ready(function() {
   console.log('this is working');
-  boo = $('#e');
-  var context = boo[0].getContext('2d');
-  context.fillStyle = "rgb(50, 50, 50)";
-  context.fillRect(10, 10, 55, 50);
+  //boo = $('#e');
+  //var ctx = boo[0].getContext('2d');
 
+  function getCanvasCtx(){
+    return $('#e')[0].getContext('2d');
+  }
+
+  function drawEli(ctx){
+    var img = new Image();
+    img.src = "http://images.nationalgeographic.com/wpf/media-live/photos/000/090/cache/african-elephant-standing_9033_600x450.jpg";
+    img.onload = function(){
+      ctx.drawImage(this,0,0,500,500);
+    }
+  }
+
+  function typeMessage(ctx, message){
+    ctx.fillStyle = 'black';
+    ctx.font = '32px sans-serif';
+    //ctx.fillText(message,50,50);
+    wrapText(ctx, message, 50, 50, 400, 40);
+  }
+
+  function clearMessage(ctx){
+    ctx.clearRect(0,0,500,500);
+    drawEli(ctx);
+  }
+
+  function wrapText(context, text, x, y, maxWidth, lineHeight){
+    var words = text.split(" ");
+    var line = "";
+    for (var n = 0; n < words.length; n++){
+      var testLine = line + words[n] + " ";
+      var metrics = context.measureText(testLine);
+      var testWidth = metrics.width;
+      if (testWidth > maxWidth) {
+        context.fillText(line, x, y);
+        line = words[n] + " ";
+        y += lineHeight;
+      }
+      else {
+        line = testLine;
+      }
+    }
+    context.fillText(line, x, y);
+  }
+
+  var ctx = getCanvasCtx();
+  drawEli(ctx);
+
+  // Bind typing enter in the text box to writing the message
+  $('.add').on("click", function(){
+    var ctx = getCanvasCtx();
+    var message = $('.message').val();
+    typeMessage(ctx, message);
+  })
+
+  // Bind clearing the message
+  $('.clear').on('click', function(){
+    var ctx = getCanvasCtx();
+    clearMessage(ctx);
+  })
 });
