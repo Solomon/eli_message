f0eb4a0bea7877f439ea7ebea4d209b036f4d42c
Solomon <solomon.kahn@gmail.com>
2012-11-05 11:08:47 -0500

    more changes before pushing frpushing from laptop

--- public/javascript.js
+++ public/javascript.js
@@ -1,24 +1,27 @@
+var current_eli = 7;
+
 $(document).ready(function() {
-  console.log('this is working');
-  //boo = $('#e');
-  //var ctx = boo[0].getContext('2d');
 
   function getCanvasCtx(){
     return $('#e')[0].getContext('2d');
   }
 
+  function getEliUrl(number){
+    return "https://s3.amazonaws.com/eli_message/elephants/" + number + ".jpeg"
+  }
+
   function drawEli(ctx){
     var img = new Image();
-    img.src = "http://images.nationalgeographic.com/wpf/media-live/photos/000/090/cache/african-elephant-standing_9033_600x450.jpg";
+    img.src = getEliUrl(current_eli);
     img.onload = function(){
       ctx.drawImage(this,0,0,450,450);
     }
   }
 
+
   function typeMessage(ctx, message){
     ctx.fillStyle = '#25020F';
     ctx.font = '18px sans-serif';
-    //ctx.fillText(message,50,50);
     wrapText(ctx, message, 50, 50, 350, 27);
   }
 
@@ -53,12 +56,17 @@ $(document).ready(function() {
     }
   }
 
+  function newEli(ctx){
+    var number = Math.floor(Math.random()*11)+1;
+    current_eli = number;
+    drawEli(ctx);
+  }
+
   var ctx = getCanvasCtx();
   drawEli(ctx);
 
   // Bind typing enter in the text box to writing the message
   $('.add').on("click", function(){
-    console.log('working');
     var ctx = getCanvasCtx();
     var message = $('.message').val();
     typeMessage(ctx, message);
@@ -68,5 +76,13 @@ $(document).ready(function() {
   $('.clear').on('click', function(){
     var ctx = getCanvasCtx();
     clearMessage(ctx);
+  })
+
+  // Get a new Eli
+  $('.new_eli').on('click', function(){
+    var number = Math.floor(Math.random()*10)+1;
+    current_eli = number;
+    var ctx = getCanvasCtx()
+    newEli(ctx)
   })
 });
